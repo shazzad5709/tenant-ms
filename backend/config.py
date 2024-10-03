3848c8fcfb804f8dad36197659f2175b175821f6
shazzad5709 <shazzad1111@gmail.com>
2024-10-04 02:01:49 +0600

    Fix db connection error

--- backend/config.py
+++ backend/config.py
@@ -9,3 +9,9 @@ uri = os.environ.get("MONGODB_URI")
 
 client = AsyncIOMotorClient(uri)
 db = client[os.environ.get("MONGODB_DATABASE")]
+
+try:
+  client.admin.command("ping")
+  print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+  print(e)
